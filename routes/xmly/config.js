4e59d47bf7135cd8f5c6088d0151f4409e79130d
youling4438 <youling0106@163.com>
2023-12-10 14:20:40 +0800

    first commit for ng-music-api

---
+++ routes/xmly/config.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://www.ximalaya.com/revision'
+module.exports = {
+  BASE_URL,
+  authKey: 'token',
+  account: {
+    name: 'Thomas',
+    phone: '15829269880',
+    password: 'angular10'
+  }
+}
